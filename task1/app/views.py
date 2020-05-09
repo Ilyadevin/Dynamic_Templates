26836a409c9bd55b2822fb0eeb757611d2d4471a
Ilya Erlingas <Aligruit@gmail.com>
2020-05-09 19:24:21 +0300

    adding CSVREADER

--- task1/app/views.py
+++ task1/app/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+import csv
+
 
 def inflation_view(request):
     template_name = 'inflation.html'
-
-    # чтение csv-файла и заполнение контекста
-    context = {}
+    with open('task1/inflation_russia.csv', 'r', encoding='utf8', ) as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            context = {row['год']: [row['Янв'], row['Фев'], row['Мар'], row['Апр'], row['Май'], row['Июн'], row['Июл'],
+                                    row['Авг'], row['Сен'], row['Окт'], row['Ноя'], row['Дек'], row['Суммарная']]}
 
     return render(request, template_name,
                   context)
