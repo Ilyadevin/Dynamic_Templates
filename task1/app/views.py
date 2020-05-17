81f78a2ce0ff12f9af1924383749b02bd6283ec0
Ilya Erlingas <Aligruit@gmail.com>
2020-05-17 03:02:40 +0300

    adding csv_parce

--- task1/app/views.py
+++ task1/app/views.py
@@ -4,11 +4,15 @@ import csv
 
 def inflation_view(request):
     template_name = 'inflation.html'
-    with open('task1/inflation_russia.csv', 'r', encoding='utf8', ) as csv_file:
+    list_of_percents = list
+    with open('inflation_russia.csv', 'r', encoding='utf8', ) as csv_file:
         reader = csv.DictReader(csv_file)
         for row in reader:
-            context = {row['год']: [row['Янв'], row['Фев'], row['Мар'], row['Апр'], row['Май'], row['Июн'], row['Июл'],
-                                    row['Авг'], row['Сен'], row['Окт'], row['Ноя'], row['Дек'], row['Суммарная']]}
-
+            list_of_percents.append(
+                dict(Год=row['Год'], Янв=row['Янв'], Фев=row['Фев'], Мар=row['Мар'], Апр=row['Апр'], Май=row['Май'],
+                     Июн=row['Июн'], Июл=row['Июл'], Авг=row['Авг'], Сен=row['Сен'], Окт=row['Окт'], Ноя=row['Ноя'],
+                     Дек=row['Дек'], Суммарная=row['Суммарная']))
+    context = {
+        }
     return render(request, template_name,
                   context)
