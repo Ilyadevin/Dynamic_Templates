1aca187ff050f7163cf137d1fce5a5b92d243b36
Ilya Erlingas <Aligruit@gmail.com>
2020-05-18 14:14:30 +0300

    update task1

--- task1/app/views.py
+++ task1/app/views.py
@@ -4,15 +4,17 @@ import csv
 
 def inflation_view(request):
     template_name = 'inflation.html'
-    list_of_percents = list
+    list_of_percents = list()
     with open('inflation_russia.csv', 'r', encoding='utf8', ) as csv_file:
-        reader = csv.DictReader(csv_file)
+        reader = csv.DictReader(csv_file, delimiter=';')
         for row in reader:
             list_of_percents.append(
-                dict(Год=row['Год'], Янв=row['Янв'], Фев=row['Фев'], Мар=row['Мар'], Апр=row['Апр'], Май=row['Май'],
-                     Июн=row['Июн'], Июл=row['Июл'], Авг=row['Авг'], Сен=row['Сен'], Окт=row['Окт'], Ноя=row['Ноя'],
-                     Дек=row['Дек'], Суммарная=row['Суммарная']))
-    context = {
-        }
+                {'year': row['Год'], 'jan': row['Янв'], 'feb': row['Фев'], 'mar': row['Мар'], 'apr': row['Апр'],
+                 'may': row['Май'], 'jun': row['Июн'], 'jul': row['Июл'], 'aug': row['Авг'], 'sep': row['Сен'],
+                 'oct': row['Окт'], 'nov': row['Ноя'], 'des': row['Дек'], 'sum': row['Суммарная']
+                 }
+            )
+    context = {'inflation': list_of_percents,
+               }
     return render(request, template_name,
                   context)
